ad2e88ae4cb65d9ee8994a0bf38839ab7ddef469
Ali Yüce <aliyucee85@gmail.com>
2021-12-08 21:03:24 +0300

    ali

---
+++ CoreDeneme/Model/Profile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDeneme.Model
+{
+    [Table(name: "Profile")]
+    public class Profile
+    {
+        [Key]
+        public long ID { get; set; }
+        [Column(TypeName = "nvarchar(500)")]
+        [Required]
+        public string Customer_Name { get; set; }
+        [Column(TypeName = "nvarchar(500)")]
+        [Required]
+        public string Mobile { get; set; }
+        [Column(TypeName = "nvarchar(MAX)")]
+        [Required]
+        public string Email { get; set; }
+        [Column(TypeName = "nvarchar(500)")]
+        [Required]
+        public string Password { get; set; }
+        [Column(TypeName = "datetime")]
+        [Required]
+        public DateTime InsertedDate { get; set; }
+        [Column(TypeName = "datetime")]
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+        [Column(TypeName = "int")]
+        public long InsertedId { get; set; }
+        [Column(TypeName = "int")]
+        public long ModifiedId { get; set; }
+        [Column(TypeName = "bit")]
+        public bool Active { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Status { get; set; }
+        [Column(TypeName = "bit")]
+        public bool IsAvailable { get; set; }
+
+    }
+}
